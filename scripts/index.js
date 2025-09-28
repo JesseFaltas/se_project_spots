ed867e5a055a27d21dac85cb1405b83c1d0fb886
Jesse Faltas <jessefaltas@gmail.com>
2025-09-27 20:41:08 -0700

    added transitions to modals

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,50 @@
+// edit elements
 const editProfileButton = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__description");
+
+const editProfileNameInput = editProfileModal.querySelector(
+  "#profile-name-input"
+);
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input"
+);
 
 editProfileButton.addEventListener("click", () => {
-  editProfileModal.style.visibility = "visible";
+  editProfileNameInput.value = profileNameEl.textContent;
+  editProfileDescriptionInput.value = profileDescriptionEl.textContent;
+  editProfileModal.classList.add("modal__is-opened");
 });
 
 editProfileCloseBtn.addEventListener("click", () => {
-  editProfileModal.style.visibility = "hidden";
+  editProfileModal.classList.remove("modal__is-opened");
 });
 
 editProfileModal.addEventListener("click", (e) => {
   if (e.target === editProfileModal) {
-    editProfileModal.style.visibility = "hidden";
+    editProfileModal.classList.remove("modal__is-opened");
   }
 });
+
+function handleEditProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileNameEl.textContent = editProfileNameInput.value;
+  profileDescriptionEl.textContent = editProfileDescriptionInput.value;
+  editProfileModal.style.visibility = "hidden";
+}
+editProfileForm.addEventListener("submit", handleEditProfileFormSubmit);
+
+//new post elements
 
 const newPostButton = document.querySelector(".profile__add-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
+const newPostForm = newPostModal.querySelector(".modal__form");
+const newPostImageInput = newPostModal.querySelector("#card-image-input");
+const newPostCaptionInput = newPostModal.querySelector("#card-caption-input");
 
 newPostButton.addEventListener("click", function () {
   newPostModal.classList.add("modal__is-opened");
@@ -27,3 +53,12 @@ newPostButton.addEventListener("click", function () {
 newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal__is-opened");
 });
+
+function handleNewPostFormSubmit(evt) {
+  evt.preventDefault();
+  console.log("New post form submitted");
+  newPostForm.reset();
+  newPostModal.classList.remove("modal__is-opened");
+}
+
+newPostForm.addEventListener("submit", handleNewPostFormSubmit);
